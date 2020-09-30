7d4f7ae32904743ae2948e9646dcc2341db03911
Baher-Essam <baher.essam@gmail.com>
2020-09-30 08:45:45 +0200

    replace mixitup with jquery toggle

--- js/index.js
+++ js/index.js
@@ -13,9 +13,26 @@ $(function() {
 });
 
 
-/*mix it up libaray initialize function*/
-var mixer = mixitup('#projects .row', {
-  selectors: {
-      target: '.mix'
-  }
-});
+/*start of projects filter */
+$(document).ready(function(){
+  //defult active list *all*
+  $('.list').first().addClass('active');
+  
+  $('.list').click(function(){
+    const projectList = $(this).attr('data-filter');
+    if(projectList == 'all')
+    {
+      $('.project').show('300');
+    }
+    else
+    {
+      $('.project').not('.'+projectList).hide('300');
+      $('.project').filter('.'+projectList).show('300');
+    }
+  })
+  // add active class on selected button
+  $('.list').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+})
+/*end of projects filter */
