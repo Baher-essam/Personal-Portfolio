806311c79a1806ba4d7742256e38a115e6694eb7
Baher-Essam <baher.essam@gmail.com>
2020-10-08 06:16:42 +0200

    Update chat plug link

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5f7e8bf14704467e89f59c2c/default';
+s1.src='https://embed.tawk.to/5f7e8bf14704467e89f59c2c/1ek36mer1';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
