a1c762f78cb63484384e68fa1afd03fdac26dab8
Baher-Essam <baher.essam@gmail.com>
2020-10-08 05:52:42 +0200

    Add live chat plugin

--- js/index.js
+++ js/index.js
@@ -36,3 +36,14 @@ $(document).ready(function(){
   })
 })
 /*end of projects filter */
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5f7e8bf14704467e89f59c2c/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
