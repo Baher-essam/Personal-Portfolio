6cb8c407a979b48b55793302e617f50128487644
Baher Essam <44957599+Baher-essam@users.noreply.github.com>
2020-12-20 00:09:41 +0200

    Update time

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('DEC 20, 2020 00:00:00').getTime(),
+let countDown = new Date('DEC 22, 2020 00:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
