95f0f9e7c48e819cbb434068b15bf571a43536f6
Baher-Essam <baher.essam@gmail.com>
2021-12-25 18:50:18 +0200

    updating fake-data content

--- src/fake-database/about/about.js
+++ src/fake-database/about/about.js
@@ -1,40 +1,49 @@
-import profile from '../../Assets/images/profile.webp'
+import profile from '../../Assets/images/profile.png';
+import profileWebp from '../../Assets/images/profile.webp';
+import introImg from '../../Assets/images/frame2.png'
+import introImgwebp from '../../Assets/images/frame2.webp'
+import github from '../../Assets/badges/github.svg'
+import linkedin from '../../Assets/badges/linkedin.svg'
+import codepen from '../../Assets/badges/codepen.svg'
 
 const aboutData = {
         "name" : "Baher Essam",
-        "Adderss" : "Cairo, Egypt.",
-        "Phone" : "01146786272",
-        "pref" : "this is a data about me",
+        "adderss" : "Cairo, Egypt.",
+        "phone" : "(+02) 01146786272",
+        'email': 'baher.essam@gmail.com',
+        'age': 25,
+        "job" : [ "Freelancer", "Web Developer" ],
+        "skills" : ["Html5", "CSS3",'Bootstrap4','Matrtlize.css','Tailwind css', "Javascript", 'Jquery',  "react-bootstrap", "Reactjs", 'JSX', 'PWA', 'laravel',"WIX / EditorX" , "Wordpress"  ],
         "photo" : profile,
+        "photoWebp" : profileWebp,
+        "photoAlt" : 'profile-photo',
+        "introImg" : introImg,
+        "introImgwebp" : introImgwebp,
+        "introImgAlt" : 'intro-photo',
+        "pref" : `
+            Web development has been always my passion. I still remember coding my first line of code and making it print “hello world!”. It was almost magic, being able to get the computer to do whatever I wanted.
+
+            I love applying responsive design with a nice animation and watching my web pages shrink into mobile screens and still look amazing. It’s oddly satisfying.`,
         "Social" : [
             {
-                'name': 'gitgub',
-                "photo" : "path",
-                "url" : "https://test.com"
+                'id':1,
+                'name': 'github',
+                "photo" : github,
+                "url" : "https://github.com/Baher-essam"
             },
             {
-                'name': 'gitgub',
-                "photo" : "path",
-                "url" : "https://test.com"
+                'id':2,
+                'name': 'linkedin',
+                "photo" : linkedin,
+                "url" : "https://www.linkedin.com/in/baher-essam/"
             },
             {
-                'name': 'gitgub',
-                "photo" : "path",
-                "url" : "https://test.com"
-            },
-            {
-                'name': 'gitgub',
-                "photo" : "path",
-                "url" : "https://test.com"
-            },
-            {
-                'name': 'gitgub',
-                "photo" : "path",
-                "url" : "https://test.com"
+                'id':3,
+                'name': 'codepen',
+                "photo" : codepen,
+                "url" : "https://codepen.io/Baher-Essam"
             }
-        ],
-        "job" : [ "freelancer", "Web Developer" ],
-        "skills" : [ "html", "css", "js" ],
+        ]
 }
 
 export default aboutData
--- src/fake-database/projects/projects.js
+++ src/fake-database/projects/projects.js
@@ -1,112 +1,156 @@
 import project1 from '../../Assets/images/project-1.webp'
-import project2 from '../../Assets/images/project-2.webp'
-import project3 from '../../Assets/images/project-3.webp'
+import project3 from '../../Assets/images/project-3.webp'//not exist
 import project4 from '../../Assets/images/project-4.webp'
 import project5 from '../../Assets/images/project-5.webp'
 import project6 from '../../Assets/images/project-6.webp'
 import project7 from '../../Assets/images/project-7.webp'
 import project8 from '../../Assets/images/project-8.webp'
 import project9 from '../../Assets/images/project-9.webp'
+import project10 from '../../Assets/images/project-11.webp'
+import project11 from '../../Assets/images/project-10.webp'
+
+import projectO1 from '../../Assets/images/project-1.jpg'
+import projectO3 from '../../Assets/images/project-3.jpg'//not exist
+import projectO4 from '../../Assets/images/project-4.jpg'
+import projectO5 from '../../Assets/images/project-5.jpg'
+import projectO6 from '../../Assets/images/project-6.jpg'
+import projectO7 from '../../Assets/images/project-7.jpg'
+import projectO8 from '../../Assets/images/project-8.jpg'
+import projectO9 from '../../Assets/images/project-9.jpg'
+import projectO10 from '../../Assets/images/project-11.jpg'
+import projectO11 from '../../Assets/images/project-10.jpg'
+
 
 const projectsData =[
     {
         "id" : 1,
+        "imagOe" : projectO1,
         "image" : project1,
-        "languages" : [ "html", "css", "js" ],
-        "name" : "project 1",
-        "status" : "live",//offline
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript", 'Jquery' ],
+        "name" : "Coder bits",
+        "status" : "offline",//offline-upgrading-pending-enhancing-compeleted-finished
         "tag" : "homepage",
         "text" : "this is a project",
-        "type" : "freelance"
+        "category" : "self",
+        "imageAlt" : "Coder-bits project",
+        "url" : null,
       }, 
       {
         "id" : 2,
-        "image" : project2,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
-        "status" : "live",
+        "imagOe" : projectO3,
+        "image" : project3,
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript", 'Jquery' ],
+        "name" : "project 3",
+        "status" : "offline",
         "tag" : "allprojects",
         "text" : "this is a project2",
-        "type" : "work",
+        "category" : "self",
         "imageAlt" : "alt",
+        "url" : null,
       }, 
       {
         "id" : 3,
-        "image" : project3,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
-        "status" : "live",
-        "tag" : "allprojects",
-        "text" : "this is a project2",
-        "type" : "work",
-        "imageAlt" : "alt",
-      }, 
-      {
-        "id" : 4,
+        "imagOe" : projectO4,
         "image" : project4,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript", 'Jquery' ],
+        "name" : "Flyo landpage",
         "status" : "live",
         "tag" : "allprojects",
         "text" : "this is a project2",
-        "type" : "work",
-        "imageAlt" : "alt",
+        "category" : "self",
+        "imageAlt" : "Flyo project",
+        "url" : "https://baher-flyo.netlify.app/",
+      }, 
+      {       
+        "id" : 4,
+        "imagOe" : projectO5,
+        "image" : project5,
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript", 'Jquery' ],
+        "name" : "Scenic",
+        "status" : "live",
+        "tag" : "allprojects",
+        "text" : "this is a project2",
+        "category" : "self",
+        "imageAlt" : "Scenic project",
+        "url" : "https://baher-scenic.netlify.app/",
       }, 
       {       
         "id" : 5,
-        "image" : project5,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
+        "imagOe" : projectO6,
+        "image" : project6,
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript", 'Jquery', 'php', 'mySQL', 'laravel' ],
+        "name" : "Rowadun",
         "status" : "live",
         "tag" : "allprojects",
         "text" : "this is a project2",
-        "type" : "work",
-        "imageAlt" : "alt",
+        "category" : "work",
+        "imageAlt" : "Rowadun project",
+        "url" : "https://www.rowadun.com/",
       }, 
       {       
         "id" : 6,
-        "image" : project6,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
+        "imagOe" : projectO7,
+        "image" : project7,
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript", 'Jquery', 'php', 'mySQL', 'laravel' ],
+        "name" : "Attractive car",
         "status" : "live",
         "tag" : "allprojects",
         "text" : "this is a project2",
-        "type" : "work",
-        "imageAlt" : "alt",
+        "category" : "work",
+        "imageAlt" : "Attractive-car project",
+        "url" : "https://www.attcar.com/",
       }, 
       {       
         "id" : 7,
-        "image" : project7,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
+        "imagOe" : projectO8,
+        "image" : project8,
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript", 'Jquery', 'php', 'mySQL', 'laravel' ],
+        "name" : "Akwan media",
         "status" : "live",
         "tag" : "allprojects",
         "text" : "this is a project2",
-        "type" : "work",
-        "imageAlt" : "alt",
+        "category" : "work",
+        "imageAlt" : "Akwan-media project",
+        "url" : "https://www.akwanmedia.com/ar",
       }, 
       {       
         "id" : 8,
-        "image" : project8,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
+        "imagOe" : projectO9,
+        "image" : project9,
+        "tech" : [ "WIX / EditorX" ],
+        "name" : "advalue",
+        "status" : "completed",
+        "tag" : "allprojects",
+        "text" : "this is a project2",
+        "category" : "freelance",
+        "imageAlt" : "advalue project",  
+        "url" : null,
+      },
+      {
+        "id" : 9,
+        "imageO" : projectO10,
+        "image" : project10,
+        "tech" : [ "Html5", "CSS3",'Bootstrap4', "Javascript" ],
+        "name" : "Madz",
         "status" : "live",
         "tag" : "allprojects",
         "text" : "this is a project2",
-        "type" : "work",
-        "imageAlt" : "alt",
+        "category" : "freelance",
+        "imageAlt" : "Madz project",
+        "url" : 'https://madz.agency/',
       }, 
       {       
-        "id" : 9,
-        "image" : project9,
-        "languages" : [ "html", "css", "js", "reactjs" ],
-        "name" : "project 2",
-        "status" : "live",
-        "tag" : "allprojects",
-        "text" : "this is a project2",
-        "type" : "work",
-        "imageAlt" : "alt",
+        "id" : 10,
+        "imageO" : projectO11,
+        "image" : project11,
+        "tech" : [ "Html5", "CSS3", "react-bootstrap", "Reactjs", 'JSX', 'PWA'],
+        "name" : "macline",
+        "status" : "building",
+        "category" : "freelance",
+        "imageAlt" : "macline project",
+        "url" : null,
       }
+
 ]
 
 export default projectsData;
