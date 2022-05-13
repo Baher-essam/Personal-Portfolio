2134862e49c9ce5280d2ff62ae25d2932546d942
Baher-Essam <baher.essam@gmail.com>
2022-05-13 20:13:01 +0200

    adding img width at intro & comments & Project Sec

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,24 +3,25 @@ import Typical from 'react-typical'
 import './home.css'
 import About from '../about/about'
 import Services from '../services/services'
-import {aboutData} from '../../fake-database/index'
+import Projects from '../projects/projects'
+import {aboutData } from '../../fake-database/index'
 import bg2 from '../../Assets/images/shape4.gif'
-
 
 
 
 const Home = () => {
     return (
         <main>
+            {/*start of intro seciton*/}
             <section id="intro" className="intro d-flex justify-content-center align-items-center">
                 <div className="container">
                     <div className="intro-content">
                         <div className="intro-art text-center">
-                            <img src={bg2} alt="intro image" className="img-fluid intro-img1"/>
+                            <img src={bg2} alt="intro image" className="img-fluid intro-img1" width={200} height={200}/>
                             <div className="intro-img2">
                                 <picture>
                                     <source srcSet={aboutData.introImgwebp} alt={aboutData.introImgAlt} className="img-fluid"/>
-                                    <img src={aboutData.introImg} alt={aboutData.introImgAlt} className="img-fluid"/>
+                                    <img src={aboutData.introImg} alt={aboutData.introImgAlt} className="img-fluid" width={184} height={184}/>
                                 </picture>
                             </div>
                         </div>
@@ -47,7 +48,7 @@ const Home = () => {
                                     {aboutData.Social.map( social =>(
                                         <li key={social.id}>
                                             <a href={social.url}  rel="noreferrer noopener" target="blank">
-                                                <img src={social.photo} alt={social.name} />
+                                                <img src={social.photo} alt={social.name} width={25} height={30} />
                                             </a>
                                         </li>
                                     ))}  
@@ -59,56 +60,63 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <About/>
-            <Services/>
+            {/*end of intro seciton*/}
 
+            {/*About seciton*/}
+            <About/>
+            {/*Services seciton*/}
+            <Services/>
+            {/*projects seciton*/}
+            <Projects/>
+            {/*contact seciton*/}
             <section id="contact" className="mb40 mt70 ">
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-12  text-capitalize text-center">
-                        <div className="section-title contact-title">
-                            <h1><span><svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill="#AB05F2" d="M50.7,-39.1C65.4,-21.9,77,-0.8,74.5,19.8C72,40.4,55.5,60.6,34.9,69.3C14.3,78,-10.3,75.2,-31.8,65.1C-53.3,55.1,-71.8,37.9,-74.4,19.1C-76.9,0.4,-63.5,-19.8,-48.5,-37C-33.4,-54.3,-16.7,-68.6,0.6,-69.1C18,-69.6,35.9,-56.3,50.7,-39.1Z" transform="translate(100 100)" />
-                                    </svg></span>feeling convinced
-                            </h1>
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-12  text-capitalize text-center">
+                            <div className="section-title contact-title">
+                                <h1><span><svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill="#AB05F2" d="M50.7,-39.1C65.4,-21.9,77,-0.8,74.5,19.8C72,40.4,55.5,60.6,34.9,69.3C14.3,78,-10.3,75.2,-31.8,65.1C-53.3,55.1,-71.8,37.9,-74.4,19.1C-76.9,0.4,-63.5,-19.8,-48.5,-37C-33.4,-54.3,-16.7,-68.6,0.6,-69.1C18,-69.6,35.9,-56.3,50.7,-39.1Z" transform="translate(100 100)" />
+                                        </svg></span>feeling convinced
+                                </h1>
+                            </div>
                         </div>
-                    </div>
-              
-                    <div className="col-sm-12">
-                        <div className="contact-form  mt50">
-                            <form className="form" id="myForm">
-                                <input type="hidden" name="form-name" value="contact"/>
-                                <div className="form-group">
-                                    <input type="text" name="name" required  className="form-control"/>
-                                    <label htmlFor="name" className="label-content">
-                                        <span className="span-data">Name</span>
-                                    </label>
-                                </div>
-                                <div className="form-group">
-                                    <input type="email" name="email" required  className="form-control"/>
-                                    <label htmlFor="email" className="label-content">
-                                        <span className="span-data">Email</span>
-                                    </label>
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" name="number" required  className="form-control"/>
-                                    <label htmlFor="number" className="label-content">
-                                        <span className="span-data">Number</span>
-                                    </label>
-                                </div>
-                                <div className="form-group">
-                                <label htmlFor="message" className="message">Message</label>
-                                <textarea name="message" rows="4" placeholder="Enter Message..." className="form-control"></textarea>
-                                </div>
-                                <div className="text-center">
-                                    <button type="submit" className="text-capitalize px60 main-btn mt10 py5">Send Message</button>
-                                </div>
-                            </form> 
+                
+                        <div className="col-sm-12">
+                            <div className="contact-form  mt50">
+                                <form className="form" id="myForm">
+                                    <input type="hidden" name="form-name" value="contact"/>
+                                    <div className="form-group">
+                                        <input type="text" name="name" required  className="form-control"/>
+                                        <label htmlFor="name" className="label-content">
+                                            <span className="span-data">Name</span>
+                                        </label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="email" name="email" required  className="form-control"/>
+                                        <label htmlFor="email" className="label-content">
+                                            <span className="span-data">Email</span>
+                                        </label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="number" required  className="form-control"/>
+                                        <label htmlFor="number" className="label-content">
+                                            <span className="span-data">Number</span>
+                                        </label>
+                                    </div>
+                                    <div className="form-group">
+                                    <label htmlFor="message" className="message">Message</label>
+                                    <textarea name="message" rows="4" placeholder="Enter Message..." className="form-control"></textarea>
+                                    </div>
+                                    <div className="text-center">
+                                        <button type="submit" className="text-capitalize px60 main-btn mt10 py5">Send Message</button>
+                                    </div>
+                                </form> 
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+
 
         </main>
     )
